eddde4777c48f5592bbc8496826e74468f7ba46c
Chris <chris.a.broome@gmail.com>
2013-10-20 19:07:37 -0400

    re-jekyllizing

--- js/app.js
+++ js/app.js
@@ -4,8 +4,25 @@ define(
     ],
     function ( Marionette ) {
         
-
         var app = new Marionette.Application();
+        
+        app.addRegions ({
+            
+            skillList: 'ul.skill-list',
+            
+            experienceWork: '.experience-work',
+            
+            experienceProjects: '.experience-projects'
+        
+        });
+        
+    
+        
+        app.on( 'start', function() {
+
+            
+
+        } );
         
         return app;
     }
--- js/main.js
+++ js/main.js
@@ -51,6 +51,8 @@ require(
     ],
     function ( app ) {
         
+
+        
         app.start();
         
     }
