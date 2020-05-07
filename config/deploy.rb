45eb0145c48f0a90d64778707cafab2b84968eb6
Chris <chris.a.broome@gmail.com>
2020-05-07 06:46:57 -0400

    Updated minimum capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "~> 3.14.0"
 
 set :application, "christopherbroome"
 set :repo_url, "git@github.com:cbroome/homepage.git"
